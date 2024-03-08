55e5edc8a74c9f7e2fc392184e298e3177e5cac7
Ugo Chinememerem <ugofranklin22@gmail.com>
2024-03-08 15:22:35 +0100

    add:new token

--- constants.js
+++ constants.js
@@ -1,7 +1,6 @@
 // Constants.js
 module.exports = {
-  MY_TOKEN: "7120237220:AAFbHk5oCyzD2NxxEZ24RfDqWMKCR2za610",
+  MY_TOKEN: "7120237220:AAE7_b5jXXaDUrb_o08Nn3EIcx3wx7qkIDc",
   BASE_URL: "https://api.telegram.org",
   PORT: process.env.PORT || 9000,
-  MONGO_URL: process.env.MONGO_URL,
 };
