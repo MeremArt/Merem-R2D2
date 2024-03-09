54b020296a45bbbe4e215bb568fd00d5e35eae02
Ugo Chinememerem <ugofranklin22@gmail.com>
2024-03-09 17:31:27 +0100

    add:health check

--- index.js
+++ index.js
@@ -2,8 +2,23 @@ const express = require("express");
 const app = express();
 const { handler } = require("./controller/index");
 const { PORT } = require("./constants");
+const axios = require("axios");
 require("dotenv").config();
+
 app.use(express.json());
+
+const serverUrl = `https://merem-r2d2.onrender.com`;
+
+const checkServerHealth = () => {
+  axios
+    .get(serverUrl)
+    .then((response) => {
+      console.log(`Server is healthy`, response.data);
+    })
+    .catch((error) => {
+      console.error(`Error checking server health:`, error.message);
+    });
+};
 
 app.post("*", async (req, res) => {
   try {
@@ -15,9 +30,10 @@ app.post("*", async (req, res) => {
   }
 });
 
-app.get("*", async (req, res) => {
-  res.send("Hello world");
-});
+const interval = 50000;
+setInterval(checkServerHealth, interval);
+
+checkServerHealth();
 
 app.listen(PORT, () => {
   console.log(`Server is running at http://localhost:${PORT}`);
